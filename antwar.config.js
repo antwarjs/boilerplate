c4360bddb043157bfa47be38438a3c4870e8ff43
Juho Vepsalainen <bebraw@gmail.com>
2015-03-15 19:58:10 +0200

    Drop `site` to match current API

--- antwar.config.js
+++ antwar.config.js
@@ -2,25 +2,23 @@
 
 
 module.exports = {
-  site: {
-    output: 'build',
-    name: 'Antwar Boilerplate',
-    author: {
-      name: 'Dr A N Twar',
-      email: 'antwar@antwar.com'
-    },
-    deploy: {
-      branch: 'gh-pages',
-    },
-    theme: {
-      name: 'antwar-default-theme',
-      navigation: [
-        {title: 'Home', path: '/'},
-        {title: 'Blog', path: '/blog'}
-      ],
-      analyticsId: 'UA-XXXXXX-1',
-      customStyles: 'specific.scss'
-    }
+  output: 'build',
+  name: 'Antwar Boilerplate',
+  author: {
+    name: 'Dr A N Twar',
+    email: 'antwar@antwar.com'
+  },
+  deploy: {
+    branch: 'gh-pages',
+  },
+  theme: {
+    name: 'antwar-default-theme',
+    navigation: [
+      {title: 'Home', path: '/'},
+      {title: 'Blog', path: '/blog'}
+    ],
+    analyticsId: 'UA-XXXXXX-1',
+    customStyles: 'specific.scss'
   }
 };
 
