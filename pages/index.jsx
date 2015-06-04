ce8a12e07a51d5b1e462e05932c64daa6ee62710
Juho Vepsalainen <bebraw@gmail.com>
2015-06-04 20:53:15 +0300

    Fix formatting

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default class Note extends React.Component {
       <div className='post post--front'>
         <h1 className='post__heading'>Welcome home!</h1>
         <p>This is the front page. Do whatever you like with it.</p>
-        <p>If you are into Coffee, rename as `index.coffee` and knock yourself out. Also Markdown etc. work as long as Webpack can load them.</p>
+        <p>If you are into Coffee, rename as <b>index.coffee</b> and knock yourself out. Also Markdown etc. work as long as Webpack can load them.</p>
       </div>
     );
   }
