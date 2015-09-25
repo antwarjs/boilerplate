ff9b51874737059ef2bc861e431b74efd9b3d054
Juho Vepsalainen <bebraw@gmail.com>
2015-09-25 16:10:07 +0300

    Drop coffeescript dependency
    It's better to leave dealing with that to the users.

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,6 @@ export default class Note extends React.Component {
       <div className='post post--front'>
         <h1 className='post__heading'>Welcome home!</h1>
         <p>This is the front page. Do whatever you like with it.</p>
-        <p>If you are into Coffee, rename as <b>index.coffee</b> and knock yourself out. Also Markdown etc. work as long as Webpack can load them.</p>
       </div>
     );
   }
